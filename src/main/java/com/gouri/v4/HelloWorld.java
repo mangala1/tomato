f0cfe99d7b58bea5b1a738b43d0308b53f937b0c
gouri <gouri@mail.com>
2017-03-03 13:02:54 -0800

    added package

--- src/main/java/com/gouri/v4/HelloWorld.java
+++ src/main/java/com/gouri/v4/HelloWorld.java
@@ -1,9 +1,10 @@
-//package com.gouri.v4;
+package com.gouri.v4;
 public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+        System.out.println("Hai");
     }
 
 }
